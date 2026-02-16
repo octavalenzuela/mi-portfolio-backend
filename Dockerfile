0415e40d1fedbc57a016333404dbf090976a37ba
octavalenzuela <octalilvalenzuela@hotmail.com>
2026-02-16 18:20:28 -0300

    Fix

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ WORKDIR /app
 COPY . .
 RUN chmod +x gradlew
 RUN ./gradlew bootJar --no-daemon
+
 FROM eclipse-temurin:17-jdk-alpine
 WORKDIR /app
 COPY --from=build /app/build/libs/*.jar app.jar
+EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/octa/backendportfolio/service/EmailService.java
+++ src/main/java/com/octa/backendportfolio/service/EmailService.java
@@ -1,7 +1,8 @@
 package com.octa.backendportfolio.service;
 
 import com.octa.backendportfolio.dto.ContactRequest;
-import lombok.Value;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -14,30 +15,39 @@ import java.util.Map;
 @Service
 public class EmailService {
 
+    @Value("${resend.api.key}")
     private String apiKey;
 
-    public void sendEmail(ContactRequest request) {
-        RestTemplate restTemplate = new RestTemplate();
+    @PostConstruct
+    public void validateKey() {
+        System.out.println("RESEND API KEY cargada: " + (apiKey != null && !apiKey.isBlank()));
+    }
 
+    public void sendEmail(ContactRequest request) {
+
+        RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setBearerAuth(apiKey);
 
-
         Map<String, Object> body = new HashMap<>();
-        body.put("from", "onboarding@resend.dev");
-        body.put("to", "octavalenzuela0@gmail.com");
+        body.put("from", "Octavio <onboarding@resend.dev>");
+        body.put("to", new String[]{"octavalenzuela0@gmail.com"});
         body.put("subject", "Nuevo contacto: " + request.getAsunto());
-        body.put("html", "<strong>De:</strong> " + request.getNombre() + " " + request.getApellido() + "<br><br>" + request.getMensaje());
+        body.put("html",
+                "<strong>Nombre:</strong> " + request.getNombre() + "<br>" +
+                        "<strong>Apellido:</strong> " + request.getApellido() + "<br><br>" +
+                        "<strong>Mensaje:</strong><br>" +
+                        request.getMensaje()
+        );
 
         HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
 
-        try {
-            restTemplate.postForEntity("https://api.resend.com/emails", entity, String.class);
-        } catch (Exception e) {
-            System.err.println("Error enviando por API: " + e.getMessage());
-            throw e;
-        }
+        restTemplate.postForEntity(
+                "https://api.resend.com/emails",
+                entity,
+                String.class
+        );
     }
 }
